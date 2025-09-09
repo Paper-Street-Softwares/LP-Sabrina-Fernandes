f8fd7f92421b7c77eff2b183f1579b442ed24ea0
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-09-09 10:07:48 -0300

    fix: Infos content

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -50,14 +50,14 @@ import { FileText } from "lucide-react";
 const currentYear = new Date().getFullYear();
 
 export const infos = {
-  name: "A_Definir",
+  name: "Sabrina Fernandes",
   email: "contato", // Email desejado pelo cliente
   emailSecundario: "A_Definir", // Email desejado pelo cliente
   domain: "sabrinafernandesjuridico.com.br", // Sem "www"
   phone: {
-    ddd: "A_Definir",
-    firstPart: "A_Definir", // Apenas os 5 primeiros números (incluindo o 9)
-    secondPart: "A_Definir", // Apenas os 4 últimos números
+    ddd: "11",
+    firstPart: "91681", // Apenas os 5 primeiros números (incluindo o 9)
+    secondPart: "1317", // Apenas os 4 últimos números
   },
   phoneSecundario: {
     ddd: "A_Definir",
